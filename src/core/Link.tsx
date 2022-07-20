6a2fff029771034824de2cc5f25a280943bb1e37
Levi R <levir620@skiff.com>
2022-07-19 21:35:01 -0700

    i hate eslint

--- src/core/Link.tsx
+++ src/core/Link.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import { css } from "@emotion/core";
 
 export interface LinkProps {
-  to: string
-  children: React.ReactNode
-  className?: string
+    to: string;
+    children: React.ReactNode;
+    className?: string;
 }
 
 export const Link = ({ to, children, className }: LinkProps) => {
-  return (
-    <a href={to} className={className}>
-      {children}
-    </a>
-  )
-}
+    return (
+        <a
+            href={to}
+            className={className}
+            css={css({
+                textDecoration: "none",
+            })}
+        >
+            {children}
+        </a>
+    );
+};
